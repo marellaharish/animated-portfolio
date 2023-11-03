3de5a623673a62ed7f3fb26bfbb0b7945c74721f
Harish <mharish@pandorarndlabs.com>
2023-11-03 15:22:01 +0530

    first commit

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,30 @@
+import styles from './style.module.scss';
+import { useInView, motion } from 'framer-motion';
+import { useRef } from 'react';
+import { slideUp, opacity } from './animation';
+import Rounded from '../../common/RoundedButton';
+export default function ndex() {
+
+    const phrase = "Empowering brands to shine in the digital age. We're here to redefine the rules and keep you at the forefront. No frills, always leading the way.";
+    const description = useRef(null);
+    const isInView = useInView(description)
+    return (
+        <div ref={description} className={styles.description}>
+            <div className={styles.body}>
+                <p>
+                    {
+                        phrase.split(" ").map((word, index) => {
+                            return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
+                        })
+                    }
+                </p>
+                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>"My unwavering dedication to design, coding, and interaction places me in a distinctive realm within the web design universe. This fusion of passions sets me apart in the digital world."</motion.p>
+                <div data-scroll data-scroll-speed={0.1}>
+                    <Rounded className={styles.button}>
+                        <p>About me</p>
+                    </Rounded>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from './style.module.scss';
+
+export default function Index() {
+  return (
+    <div className={styles.footer}>
+      <a href="https://wa.me/9848173866" target='_blank'>Whatsapp</a>
+      <a>Instagram</a>
+      <a>GitHub</a>
+      <a>LinkedIn</a>
+    </div>
+  )
+}
